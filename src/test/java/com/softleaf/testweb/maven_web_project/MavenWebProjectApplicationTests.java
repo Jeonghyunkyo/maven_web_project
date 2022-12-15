8ebb383559ed216fedec077edc1db573cecc7b75
Jeonghyunkyo <hchk11@naver.com>
2022-12-15 15:31:35 +0900

    Initial commit

---
+++ src/test/java/com/softleaf/testweb/maven_web_project/MavenWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softleaf.testweb.maven_web_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
